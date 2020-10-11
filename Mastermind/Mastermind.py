d164e4fb8021c2b8d7accbadd27989495ec3c696
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-11 21:44:50 +0200

    check egality and close when the code is found

--- Mastermind/Mastermind.py
+++ Mastermind/Mastermind.py
@@ -6,16 +6,22 @@ secretCode = [ 0 ] * CODE_SIZE
 def chooseSecretCode() :
     for i in range( CODE_SIZE ) :
         secretCode[i] = random.randint( 0, NB_COLORS - 1 )
+    print (secretCode)
 
 def analyseProposition( proposition ) :
-    return True
+    codeFound = True
+    for i in range(CODE_SIZE) :
+        if (proposition[i] != secretCode[i]) :
+            codeFound = False
+    return codeFound
 
 def askProposition() :
     proposition = [0] * CODE_SIZE
     print("Entrez votre proposition de code : ")
     print("Appuyez sur ENTREE après chaque nombre.")
     for i in range( CODE_SIZE ) :
-        proposition[i] = input ("Nombre " + str(i + 1) + " : ")
+        proposition[i] = int(input ("Nombre " + str(i + 1) + " : "))
+    return proposition
 
 def gameLoop() :
     chooseSecretCode()
@@ -25,4 +31,7 @@ def gameLoop() :
         if (analyseProposition( proposition ) == True) :
             codeFound = True
 
+def showGameWin() :
+    print ("Vous avez gagné ! Bravo !")
 gameLoop()
+showGameWin()
