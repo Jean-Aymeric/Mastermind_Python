47c4974ae060d3b448baf138079b09eec9aead10
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-11 21:36:19 +0200

    ask proposition and gameLoop

--- Mastermind/Mastermind.py
+++ Mastermind/Mastermind.py
@@ -7,5 +7,22 @@ def chooseSecretCode() :
     for i in range( CODE_SIZE ) :
         secretCode[i] = random.randint( 0, NB_COLORS - 1 )
 
-chooseSecretCode()
-print(secretCode)
+def analyseProposition( proposition ) :
+    return True
+
+def askProposition() :
+    proposition = [0] * CODE_SIZE
+    print("Entrez votre proposition de code : ")
+    print("Appuyez sur ENTREE après chaque nombre.")
+    for i in range( CODE_SIZE ) :
+        proposition[i] = input ("Nombre " + str(i + 1) + " : ")
+
+def gameLoop() :
+    chooseSecretCode()
+    codeFound = False
+    while (not codeFound) :
+        proposition = askProposition()
+        if (analyseProposition( proposition ) == True) :
+            codeFound = True
+
+gameLoop()
