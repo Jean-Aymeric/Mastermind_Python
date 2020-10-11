45d22c05d29b377094a20319e4a735f1eb1c96de
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-11 22:22:33 +0200

    test and print the well placed number

--- Mastermind/Mastermind.py
+++ Mastermind/Mastermind.py
@@ -6,23 +6,26 @@ secretCode = [ 0 ] * CODE_SIZE
 def chooseSecretCode() :
     for i in range( CODE_SIZE ) :
         secretCode[i] = random.randint( 0, NB_COLORS - 1 )
-    print (secretCode)
+    print( secretCode )
 
 def analyseProposition( proposition ) :
-    codeFound = True
-    for i in range(CODE_SIZE) :
-        if (proposition[i] != secretCode[i]) :
-            codeFound = False
-    return codeFound
+    analyseResult = [ 0, 0 ]
+    for i in range( CODE_SIZE ) :
+        if (proposition[i] == secretCode[i]) :
+            analyseResult[0] += 1
+    return analyseResult
+
+def showAnalyseResult(analyseResult) :
+    print("Nombres bien placés : " + str(analyseResult[0]))
 
 def askProposition() :
-    proposition = [-1] * CODE_SIZE
-    print("Entrez votre proposition de code : ")
-    print("Appuyez sur ENTREE après chaque nombre.")
-    print ("Chaque nombre doit être inférieur à " + str(NB_COLORS))
+    proposition = [ -1 ] * CODE_SIZE
+    print( "\nEntrez votre proposition de code : " )
+    print( "Appuyez sur ENTREE après chaque nombre." )
+    print( "Chaque nombre doit être inférieur à " + str( NB_COLORS ) )
     for i in range( CODE_SIZE ) :
        while ((proposition[i] >= NB_COLORS) or (proposition[i] < 0)) :
-           proposition[i] = int(input ("Nombre " + str(i + 1) + " : "))
+           proposition[i] = int( input( "Nombre " + str( i + 1 ) + " : " ) )
  
     return proposition
 
@@ -31,10 +34,14 @@ def gameLoop() :
     codeFound = False
     while (not codeFound) :
         proposition = askProposition()
-        if (analyseProposition( proposition ) == True) :
+        analyseResult = analyseProposition( proposition )
+        showAnalyseResult( analyseResult )
+        if (analyseResult[0] == CODE_SIZE) :
             codeFound = True
 
 def showGameWin() :
-    print ("Vous avez gagné ! Bravo !")
+    print( "-----------------------------" )
+    print( "| Vous avez gagné ! Bravo ! |" )
+    print( "-----------------------------" )
 gameLoop()
 showGameWin()
